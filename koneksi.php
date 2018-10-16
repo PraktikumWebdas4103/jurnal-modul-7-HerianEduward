1451a27e2f35bbf1bf2b9ab775728c0841a52bc8
HerianEduward <43520686+HerianEduward@users.noreply.github.com>
2018-10-16 17:09:04 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,27 @@
+<?php
+ $hostname   = "localhost";
+ $username = "root";
+ $pass     = "";
+ $db       = "inputanmahasiswa";
+ $con     = new mysqli($hostname, $username, $pass, $db);
+ if($con==false){
+  die("koneksi gagal". $con->connect_error);
+ }
+  if (isset($_POST['submit'])) {
+   $nama           =$_POST['nama'];
+   $nim            =$_POST['nim'];
+   $jeniskelamin   =$_POST['jeniskelamin'];
+   $programstudi   =$_POST['programstudi'];
+   $fakultas       =$_POST['fakultas'];
+   $asal           =$_POST['asal'];
+   $motto          =$_POST['motto'];
+ 
+ $sql="INSERT INTO inputanmahasiswa(nama,nim,jeniskelamin,programstudi,fakultas,asal,motto) VALUES ('$nama','$nim','$jeniskelamin','$programstudi','$fakultas','$asal
+ ','$motto')";
+   if(mysqli_query($con,$sql)){
+    echo "Database sudah masuk";
+   }else{
+    echo "Database Gagal";
+   }
+   }
+ ?>
